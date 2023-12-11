e9416c642b2458fcec9e5450ff485024571150d0
Lena-Jurkschat <lena.jurkschat@tu-dresden.de>
2023-12-11 11:00:46 +0100

    GPU usage setup adaptation

--- hpc_setup.sh
+++ hpc_setup.sh
@@ -8,4 +8,7 @@ module load release/23.04  GCCcore/11.3.0 Python/3.10.4
 virtualenv --system-site-packages hybridqa_env
 
 source hybridqa_env/bin/activate 
+
+#install torch version that fits to loaded CUDA Version
+pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
