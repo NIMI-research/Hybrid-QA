ab4f6a58a2e61b04623d80b781791439d932dfd8
Lena-Jurkschat <lena.jurkschat@tu-dresden.de>
2023-12-22 10:23:32 +0100

    torch ships own cuda

--- activate_env.sh
+++ activate_env.sh
@@ -3,6 +3,6 @@
 #module load modenv/hiera GCCcore/11.3.0 Python/3.10.4
 
 #barnard
-module load release/23.04  GCCcore/11.3.0 Python/3.10.4 CUDA/11.8.0
+module load release/23.04  GCCcore/11.3.0 Python/3.10.4 #CUDA/11.8.0
 
 source hybridqa_env/bin/activate
